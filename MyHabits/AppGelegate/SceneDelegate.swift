8a8c0d462e8671028cc091d3eabe59090c957982
MaybeRT-rt <maybe.rt@icloud.com>
2023-07-17 20:23:59 +0300

    refresh TabBarController and unloaded SceneDelegate

--- MyHabits/AppGelegate/SceneDelegate.swift
+++ MyHabits/AppGelegate/SceneDelegate.swift
@@ -12,44 +12,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let habitsVC = HabitsViewController()
-        let infoVC = InfoViewController()
-        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = MainTabBarController()
         
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [habitsVC, infoVC]
-        
-        window.rootViewController = createTabBar()
-        window.makeKeyAndVisible()
-        
-        self.window = window
-        
-    }
-    
-    func creatHabits() -> UINavigationController {
-        let habitsVC = HabitsViewController()
-        habitsVC.tabBarItem = UITabBarItem(title: "Привычки", image: UIImage(systemName: "list.star"), tag: 0)
-        
-        return UINavigationController(rootViewController: habitsVC)
-    }
-    
-    func creatInfo() -> UINavigationController {
-        let infoVC = InfoViewController()
-        infoVC.tabBarItem = UITabBarItem(title: "Информация", image: UIImage(systemName: "info.circle.fill"), tag: 1)
-        
-        return UINavigationController(rootViewController: infoVC)
-    }
-    
-    private func createTabBar() ->  UITabBarController {
-        
-        let tbController = UITabBarController()
-        UITabBar.appearance().backgroundColor = .white
-        UITabBar.appearance().tintColor = UIColor(red: 161/255, green: 22/255, blue: 204/255, alpha: 1.0)
-        tbController.viewControllers = [creatHabits(), creatInfo()]
-        
-        return tbController
     }
 }
-
---
+++ MyHabits/Conrollers/MainTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  MainTabBarController.swift
+//  MyHabits
+//
+//  Created by Liz-Mary on 17.07.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        UITabBar.appearance().backgroundColor = .white
+        UITabBar.appearance().tintColor = UIColor(red: 161/255, green: 22/255, blue: 204/255, alpha: 1.0)
+        creatTabBarControllers()
+    }
+    
+    private func creatTabBarControllers() {
+        viewControllers = [generateViewController(rootViewController: HabitsViewController(), image: UIImage(systemName: "list.star") ?? UIImage(), title: "Привычки"), generateViewController(rootViewController: InfoViewController(), image: UIImage(systemName: "info.circle.fill") ?? UIImage(), title: "Информация")]
+    }
+    
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
+    }
+    
+}
+
